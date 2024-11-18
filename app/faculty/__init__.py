4bee075e59ad079ba8adee64b8ef2119adab7fbd
Gabriel Momoh <gemmomoh@gmail.com>
2024-11-18 22:11:15 +0100

    fixed

--- app/faculty/__init__.py
+++ app/faculty/__init__.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 
-faculty_bp=Blueprint("faculty", __name__, template_folder="templates",static_folder="static")
 
+faculty_bp=Blueprint('faculty',__name__, template_folder="templates", static_folder= 'statics')
 
 from app.faculty import views
-
